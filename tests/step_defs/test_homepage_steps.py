64603af784ed088b97445d30e3b91f4a7d7590d0
zekedellavechia <67669609+zekedellavechia@users.noreply.github.com>
2021-01-04 09:55:23 -0300

    tideup

--- tests/step_defs/test_homepage_steps.py
+++ tests/step_defs/test_homepage_steps.py
@@ -15,11 +15,13 @@ scenarios('../features/homepage.feature')
 # Steps
 
 
+# Given
 @given('homepage is displayed')
 def home_page():
     browser.open_url(base_url)
 
 
+# When
 @when('the user clicks home')
 def click_button():
     home_button.click()
@@ -28,6 +30,47 @@ def click_button():
 @when('the user clicks results')
 def click_results():
     results_button.click()
+
+
+@when('the user clicks about')
+def user_clicks_about():
+    about_button.click()
+
+
+@when('the user clicks contact us')
+def user_clicks_contact_us():
+    contact_us_button.click()
+
+
+@when('the user clicks FAQ')
+def user_clicks_faq():
+    faq_button.click()
+
+
+@when('the user clicks resources')
+def user_clicks_resources():
+    resources_button.click()
+
+
+# Then
+@then('resources page opens')
+def resources_page_opens():
+    pass
+
+
+@when('the user clicks our partners')
+def user_clicks_our_partners():
+    our_partners_button.click()
+
+
+@then('our partners page opens')
+def our_partners_page_opens():
+    pass
+
+
+@then('contact us page opens')
+def contact_us_page_opens():
+    pass
 
 
 @then('homepage page opens')
@@ -45,51 +88,11 @@ def close_browser():
     browser.close()
 
 
-@when('the user clicks about')
-def user_clicks_about():
-    about_button.click()
-
-
-@then('about page opens')
-def about_page_opens():
-    pass
-
-
-@when('the user clicks FAQ')
-def user_clicks_faq():
-    faq_button.click()
-
-
 @then('FAQ page opens')
 def faq_page_opens():
     pass
 
 
-@when('the user clicks resources')
-def user_clicks_resources():
-    resources_button.click()
-
-
-@then('resources page opens')
-def resources_page_opens():
-    pass
-
-
-@when('the user clicks our partners')
-def user_clicks_our_partners():
-    our_partners_button.click()
-
-
-@then('our partners page opens')
-def our_partners_page_opens():
-    pass
-
-
-@when('the user clicks contact us')
-def user_clicks_contact_us():
-    contact_us_button.click()
-
-
-@then('contact us page opens')
-def contact_us_page_opens():
+@then('about page opens')
+def about_page_opens():
     pass
